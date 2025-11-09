20df0becc69208f10fbb67f20c986cbfd9d41c38
Tahrima-Banna <tahrimabannadr23@gmail.com>
2025-11-09 15:19:31 +0600

    first commit

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <header>
+                <Navbar></Navbar>
+            </header>
+            <main>
+                <Outlet></Outlet>
+            </main>
+
+            <Footer>
+          
+            </Footer>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/pages/PlantsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlantsPage = () => {
+    return (
+        <div>
+            plants...
+        </div>
+    );
+};
+
+export default PlantsPage;
